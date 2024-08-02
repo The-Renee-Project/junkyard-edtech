853f20561cfdd078ed0688d54ce69a58cdb9e0e7
OutisNoMore <OutisNoMore@protonmail.com>
2024-08-02 15:52:32 -0700

    Create graph of base rate for spec intrate benchmark

---
+++ graphs/spec_graph.py
@@ -0,0 +1,30 @@
+"""
+spec_graph
+-----------------------------------------------------------
+Create a bar graph of rates from results of running intrate
+test using runcpu2017
+-----------------------------------------------------------
+INPUTS
+  csv file with results
+-----------------------------------------------------------
+OUTPUTS
+  Bar graph
+----------------------------------------------------------
+USAGE: python3 spec_graph.py
+"""
+import pandas
+from matplotlib import pyplot
+
+if __name__ == "__main__":
+    CSV = 'spec_intrate.csv'
+    DATA = pandas.read_csv(CSV)
+    DATAFRAME = pandas.DataFrame(DATA)
+
+    BENCHMARK = DATAFRAME['Benchmark']
+    BASE_RATE = DATAFRAME['Base Rate']
+
+    _, AXES = pyplot.subplots()
+    AXES.barh(BENCHMARK, BASE_RATE)
+    AXES.set_xlabel('Base Rate')
+    AXES.set_title('Pixel Fold SPEC Intrate Test')
+    pyplot.show()
